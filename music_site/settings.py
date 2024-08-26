2b0b4af31d6dd0d76c88539691e7d007f2914761
victorsmart <victchubxy@gmail.com>
2024-08-26 08:24:09 -0700

    populating the database

--- music_site/settings.py
+++ music_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t98&y34c6sir2q5)+rq2z+63ymisif95m@eeruc^b6mt$sawud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
